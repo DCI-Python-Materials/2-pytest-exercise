4ad9b89435d1bd8047448042c56480c9037bfc5b
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-06-29 13:19:32 +0200

    set task

--- text/tools.py
+++ text/tools.py
@@ -1,8 +1,12 @@
 def upper_case(str):
     return str.upper()
-
+    
 def revers(str):
-    return str[::-1]
+    return str[::-1] 
 
 # write a function called word_count which will take one string paramater 
-# it should return
+# it should return the numbers of the words on the given string paramater
+# write test for this function which should check the following cases:
+# no parameter passed
+# wrong parameter datatype
+# wrong output 
